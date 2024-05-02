73f913cb478d4f6b6e2783250189b8fd3f8aeb51
Juliano Lopes <julopys@hotmail.com>
2024-05-01 21:10:21 -0300

    Adds doc information and url repo to buildvars.

--- buildVars.py
+++ buildVars.py
@@ -14,17 +14,31 @@ addon_info = {
         "addon_name" : "virtualNotesForNVDA",
         # Add-on summary, usually the user visible name of the addon.
         # Translators: Summary for this add-on to be shown on installation and add-on information.
-        "addon_summary" : _("Creates virtual notes to be read by NVDA"),
+        "addon_summary" : _("Virtual Notes For NVDA (creates virtual notes to be read by NVDA)"),
         # Add-on description
         # Translators: Long description to be shown for this add-on on add-on information from add-ons manager
         "addon_description" : _("""
-Creates virtual notes to be read by NVDA """),
+This Addon (Virtual Notes For NVDA
+) creates virtual notes to be read by NVDA. The following steps are available:
+* Select a text;  
+* Use shortcuts (they all start with the keys "NVDA + Control + Shift).  
+So, by pressing these keys, you can press the following letters further:  
+* A to add a new temporary note to memory;  
+* J for previous note;  
+* K for next note;  
+* L for current note;  
+* M to replace note in current position.  
+If the current note has more than one line (text copied from a text editor) use the following letters:  
+* U for previous line of current note;  
+* O for next line of current note;  
+* P for current line of current note.  
+"""),
         # version
         "addon_version" : "1.0.0",
         # Author(s)
         "addon_author" : _("Juliano Lopes"),
         # URL for the add-on documentation support
-        "addon_url" : "https://julianolopes.com.br",
+        "addon_url" : "https://github.com/juliano-lopes/virtual-notes-for-NVDA",
         # Documentation file name
         "addon_docFileName" : "readme.html",
         # Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
